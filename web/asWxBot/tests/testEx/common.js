23df448550ec93a2d7ee628017e0e803f515a317
astump <astump@astump-Desktop>
2019-10-14 17:07:35 -0500

    v17.00.2c - Interim bot updates no API changes 10/14/19

---
+++ web/asWxBot/tests/testEx/common.js
@@ -0,0 +1,7 @@
+function isSet(varIn) {
+
+	if(typeof varIn !== 'undefined' && varIn) {
+		return true;
+	} else { return false; }
+
+}
---
+++ web/asWxBot/tests/testImport.js
@@ -0,0 +1,9 @@
+import('./testEx/common.js');
+
+var thisReturnsTrue = "var is set!";
+
+if(isSet(thisReturnsTrue)) {
+	console.log("YUP!");
+} else {
+	console.log("NOPE!");
+}
