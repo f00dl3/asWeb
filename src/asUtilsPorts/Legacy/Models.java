55b4aea3c00977ef958c165675832a90bc7d55d2
astump <astump@astump-Desktop>
2020-02-05 19:21:02 -0600

    Build 200205.1920 - More cumulative updates

---
+++ src/asUtilsPorts/Legacy/Models.java
@@ -0,0 +1,162 @@
+/*
+by Anthony Stump
+Models core class
+
+Created: 22 Sep 2017
+Updated: 5 Feb 2020
+Status: Production
+DECOM ASAP - LEGACY PORT
+
+Completed: GFS, CMC, HRRR, HRWA, HRWN, NAM, SRFA, SRFN
+*/
+
+package asUtilsPorts.Legacy;
+
+import java.io.*;
+import java.sql.*;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import asUtils.Model.ModelBeans;
+import asUtilsPorts.Weather.ModelImageOps;
+import asUtilsPorts.Weather.ModelShare;
+import asUtils.Model.ModelWorker;
+import asWebRest.shared.MyDBConnector;
+import asWebRest.shared.WebCommon;
+
+public class Models {
+
+	public void initiator(boolean sysProc, String getHour) {
+
+        MyDBConnector mdb = new MyDBConnector();
+        Connection dbc = null;
+        
+        try { dbc = mdb.getMyConnection(); } catch (Exception e) { e.printStackTrace(); }
+        
+        ModelBeans modelBeans = new ModelBeans();
+        ModelImageOps mio = new ModelImageOps();
+        ModelShare ms = new ModelShare();
+        WebCommon wc = new WebCommon();                
+        
+		final DateTime tDateTime = new DateTime(DateTimeZone.UTC).minusHours(4);
+		final DateTimeFormatter getDateFormat = DateTimeFormat.forPattern("yyyyMMdd");
+		final String getDate = getDateFormat.print(tDateTime);
+		final String modelRunString = getDate+"_"+getHour+"Z";
+		final File imgOutPath = modelBeans.getImgOutPath();
+		final File xml2Path = modelBeans.getXml2Path();
+		final File mSQLDebugDumpFile = new File(xml2Path.getPath()+"/mSQLDebugDump.json");
+
+		wc.deleteDir(xml2Path);
+		imgOutPath.mkdirs();
+		
+		final String[] mw01Args = { getHour, "1" };
+		final String[] mw02Args = { getHour, "2" };
+		final String[] mw03Args = { getHour, "3" };
+		final String[] mw04Args = { getHour, "4" };
+		final String[] mw05Args = { getHour, "5" };
+		final String[] mw06Args = { getHour, "6" };
+		final String[] mw07Args = { getHour, "7" };
+		final String[] mw08Args = { getHour, "8" };
+		final String[] mw09Args = { getHour, "9" };
+		final String[] mw10Args = { getHour, "10" };
+		final String[] mw11Args = { getHour, "11" };
+		final String[] mw12Args = { getHour, "12" };
+		final String[] mw13Args = { getHour, "13" };
+		final String[] mw14Args = { getHour, "14" };
+		final String[] mw15Args = { getHour, "15" };
+		final String[] mw16Args = { getHour, "16" };
+		final String[] mw17Args = { getHour, "17" };
+		final String[] mw18Args = { getHour, "18" };
+		final String[] mw19Args = { getHour, "19" };
+		final String[] mw20Args = { getHour, "20" };
+		final String[] mw21Args = { getHour, "21" };
+		final String[] mw22Args = { getHour, "22" };
+		final String[] mw23Args = { getHour, "23" };
+		final String[] mw24Args = { getHour, "24" };
+		final String[] mw25Args = { getHour, "25" };
+		final String[] mw26Args = { getHour, "26" };
+		final String[] mw27Args = { getHour, "27" };
+		
+		Thread mw01 = new Thread(() -> { ModelWorker.main(mw01Args); });
+		Thread mw02 = new Thread(() -> { ModelWorker.main(mw02Args); });
+		Thread mw03 = new Thread(() -> { ModelWorker.main(mw03Args); });
+		Thread mw04 = new Thread(() -> { ModelWorker.main(mw04Args); });
+		Thread mw05 = new Thread(() -> { ModelWorker.main(mw05Args); });
+		Thread mw06 = new Thread(() -> { ModelWorker.main(mw06Args); });
+		Thread mw07 = new Thread(() -> { ModelWorker.main(mw07Args); });
+		Thread mw08 = new Thread(() -> { ModelWorker.main(mw08Args); });
+		Thread mw09 = new Thread(() -> { ModelWorker.main(mw09Args); });
+		Thread mw10 = new Thread(() -> { ModelWorker.main(mw10Args); });
+		Thread mw11 = new Thread(() -> { ModelWorker.main(mw11Args); });
+		Thread mw12 = new Thread(() -> { ModelWorker.main(mw12Args); });
+		Thread mw13 = new Thread(() -> { ModelWorker.main(mw13Args); });
+		Thread mw14 = new Thread(() -> { ModelWorker.main(mw14Args); });
+		Thread mw15 = new Thread(() -> { ModelWorker.main(mw15Args); });
+		Thread mw16 = new Thread(() -> { ModelWorker.main(mw16Args); });
+		Thread mw17 = new Thread(() -> { ModelWorker.main(mw17Args); });
+		Thread mw18 = new Thread(() -> { ModelWorker.main(mw18Args); });
+		Thread mw19 = new Thread(() -> { ModelWorker.main(mw19Args); });
+		Thread mw20 = new Thread(() -> { ModelWorker.main(mw20Args); });
+		Thread mw21 = new Thread(() -> { ModelWorker.main(mw21Args); });
+		Thread mw22 = new Thread(() -> { ModelWorker.main(mw22Args); });
+		Thread mw23 = new Thread(() -> { ModelWorker.main(mw23Args); });
+		Thread mw24 = new Thread(() -> { ModelWorker.main(mw24Args); });
+		Thread mw25 = new Thread(() -> { ModelWorker.main(mw25Args); });
+		Thread mw26 = new Thread(() -> { ModelWorker.main(mw26Args); });
+		Thread mw27 = new Thread(() -> { ModelWorker.main(mw27Args); });
+		Thread mwPool[] = { mw01, mw02, mw03, mw04, mw05, mw06, mw07, mw08, mw09, mw10, mw11, mw12, mw13, mw14, mw15, mw16, mw17, mw18, mw19, mw20, mw21, mw22, mw23, mw24, mw25, mw26, mw27 }; 
+		for (Thread thread : mwPool) { thread.start(); }
+		for (int i = 0; i < mwPool.length; i++) { try { mwPool[i].join(); } catch (InterruptedException nx) { nx.printStackTrace(); } }
+		
+		mio.main(getHour, xml2Path);
+		
+		String mSQLIndex = null;
+		String mSQLQuery = null;
+		
+		String hrrrJSONString = ms.jsonMerge("hrrr");
+		
+		if(getHour.equals("03") || getHour.equals("09") || getHour.equals("15") || getHour.equals("21")) {
+			String srfaJSONString = ms.jsonMerge("srfa");
+			String srfnJSONString = ms.jsonMerge("srfn");
+			mSQLIndex = "INSERT INTO WxObs.MOS_Index (RunString, GFS, NAM4KM, RAP, CMC, HRRR, HRWA, HRWN, SRFA, SRFN) VALUES ('"+modelRunString+"',0,0,0,0,1,0,0,1,1);";
+			mSQLQuery = "INSERT INTO WxObs.KOJC_MFMD (RunString, HRRR, SRFA, SRFN) VALUES ('"+modelRunString+"','"+hrrrJSONString+"','"+srfaJSONString+"','"+srfnJSONString+"');";
+		} else if(getHour.equals("00") || getHour.equals("06") || getHour.equals("12") || getHour.equals("18")) {
+			String gfsJSONString = ms.jsonMerge("gfs");
+			String namJSONString = ms.jsonMerge("nam");
+			if(getHour.equals("00") || getHour.equals("12")) {
+				String cmcJSONString = ms.jsonMerge("cmc");
+				String hrwaJSONString = ms.jsonMerge("hrwa");
+				String hrwnJSONString = ms.jsonMerge("hrwn");
+				mSQLIndex = "INSERT INTO WxObs.MOS_Index (RunString, GFS, NAM4KM, RAP, CMC, HRRR, HRWA, HRWN, SRFA, SRFN) VALUES ('"+modelRunString+"',1,1,0,1,1,1,1,0,0);";
+				mSQLQuery = "INSERT INTO WxObs.KOJC_MFMD (RunString, GFS, NAM, CMC, HRRR, HRWA, HRWN) VALUES ('"+modelRunString+"','"+gfsJSONString+"','"+namJSONString+"','"+cmcJSONString+"','"+hrrrJSONString+"','"+hrwaJSONString+"','"+hrwnJSONString+"');";
+			} else {
+				mSQLIndex = "INSERT INTO WxObs.MOS_Index (RunString, GFS, NAM4KM, RAP, CMC, HRRR, HRWA, HRWN, SRFA, SRFN) VALUES ('"+modelRunString+"',1,1,0,0,1,0,0,0,0);";
+				mSQLQuery = "INSERT INTO WxObs.KOJC_MFMD (RunString, GFS, NAM, HRRR) VALUES ('"+modelRunString+"','"+gfsJSONString+"','"+namJSONString+"','"+hrrrJSONString+"');";
+			}
+		} else {
+			mSQLIndex = "INSERT INTO WxObs.MOS_Index (RunString, GFS, NAM4KM, RAP, CMC, HRRR, HRWA, HRWN, SRFA, SRFN) VALUES ('"+modelRunString+"',0,0,0,0,1,0,0,0,0);";
+			mSQLQuery = "INSERT INTO WxObs.KOJC_MFMD (RunString, HRRR) VALUES ('"+modelRunString+"','"+hrrrJSONString+"');";
+		}
+		
+		String mSQLDumper = mSQLIndex + "\n" + mSQLQuery;
+		
+		try { wc.varToFile(mSQLDumper, mSQLDebugDumpFile, false); } catch (FileNotFoundException fnf) { fnf.printStackTrace(); }
+		
+		try { wc.q2do1c(dbc, mSQLIndex, null); } catch (Exception e) { e.printStackTrace(); }
+		try { wc.q2do1c(dbc, mSQLQuery, null); } catch (Exception e) { e.printStackTrace(); }
+                
+        try { dbc.close(); } catch (Exception e) { e.printStackTrace(); }
+		
+	}
+	
+
+	public static void main(String[] args) {
+		Models models = new Models();
+		String getHour = args[0];
+		models.initiator(true, getHour);
+	}
+
+}
---
+++ src/asUtilsPorts/Legacy/xs19.java
@@ -0,0 +1,224 @@
+/*
+XML Stations 2019 - Core Process 
+Created: 4 Sep 2016
+Updated: 4 Feb 2020
+LEGACY - DEPRECIATE WHEN asUtilsPorts.Stations WORKS
+*/
+
+package asUtilsPorts.Legacy;
+
+import asUtilsPorts.Shares.JunkyBeans;
+import asUtilsPorts.Weather.ModelBeans;
+import asUtilsPorts.Weather.xsImageOp;
+import asUtilsPorts.Weather.xsMETARAutoAdd;
+import asUtilsPorts.Weather.xsWorkerBasic;
+import asUtilsPorts.Weather.xsWorkerBouy;
+import asUtilsPorts.Weather.xsWorkerFull;
+import asUtilsPorts.Weather.xsWorkerHydro;
+import asUtilsPorts.Weather.xsWorkerMETARStream;
+import asUtilsPorts.Weather.xsWorkerWunder;
+import asWebRest.shared.MyDBConnector;
+import asWebRest.shared.WebCommon;
+
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+public class xs19 {
+
+	public static void main(String[] args) {
+            
+        final boolean debugMode = false;
+
+        MyDBConnector mdb = new MyDBConnector();
+        Connection dbc = null;
+        try { dbc = mdb.getMyConnection(); } catch (Exception e) { e.printStackTrace(); }
+
+        JunkyBeans junkyBeans = new JunkyBeans();
+        ModelBeans modelBeans = new ModelBeans();
+        WebCommon wc = new WebCommon();
+        xsImageOp xsio = new xsImageOp();
+        xsMETARAutoAdd xsmaa = new xsMETARAutoAdd();
+        xsWorkerBasic xswb = new xsWorkerBasic();
+        xsWorkerBouy xswy = new xsWorkerBouy();
+        xsWorkerFull xswf = new xsWorkerFull();
+        xsWorkerHydro xswh = new xsWorkerHydro();
+        xsWorkerMETARStream xswm = new xsWorkerMETARStream();
+        xsWorkerWunder xsww = new xsWorkerWunder();
+            
+		final long startTime = System.currentTimeMillis();
+
+		final File ramDrive = junkyBeans.getRamDrive();
+		final String xsTmp = ramDrive.getPath()+"/xsTmpJ_19";
+		final String tFHour2D = "02";
+		final String tFHour4D = "0002";
+		final String tSFHour2D = "03";
+		final String tSFHour4D = "0003";
+		final DateTime tDateTime = new DateTime(DateTimeZone.UTC).minusHours(2);
+		final DateTimeFormatter getHourFormat = DateTimeFormat.forPattern("HH");
+		final DateTimeFormatter getHourMinFormat = DateTimeFormat.forPattern("HHmm");
+		final DateTimeFormatter getDateFormat = DateTimeFormat.forPattern("yyyyMMdd");
+		final String getHour = getHourFormat.print(tDateTime);
+		final String getHourMin = getHourMinFormat.print(tDateTime);
+		final String getDate = getDateFormat.print(tDateTime);
+		final File gradsOutObj = new File(xsTmp+"/grib2/iOut");
+		final File helpers = junkyBeans.getHelpers();
+		final File jsonDebugDumpFile = new File(xsTmp+"/dbgBigString.json");
+		final File jsonDebugDumpRapidFile = new File(xsTmp+"/dbgRapidString.json");
+		final File logFile = new File(xsTmp+"/xs19.log");
+		final File metarsZipFile = new File(xsTmp+"/metars.xml.gz");
+		final File nwsObsXMLzipFile = new File(xsTmp+"/index.zip");
+		final File wwwOutObj = new File(junkyBeans.getWebRoot().toString()+"/G2Out/xsOut");
+		final File xsTmpObj = new File(xsTmp);
+		final String gVarsSQL = "SELECT gVar FROM WxObs.gradsOutType WHERE Active=1;";
+		final String gVarsHSQL = "SELECT gVar FROM WxObs.gradsOutType WHERE Active=1 AND HighRes=1;";
+		final String gVarsLSQL = "SELECT gVar FROM WxObs.gradsOutType WHERE Active=1 AND HighRes=0;";
+		final String resHigh = "13068x6600";
+		final String resLow = "2904x1440";
+		final String appPath = junkyBeans.getAppShareSys().toString();
+		final String[] xwuWorkerArgs = { xsTmp, "None" };
+		List<String> gVars = new ArrayList<String>();
+		List<String> gVarsH = new ArrayList<String>();
+		List<String> gVarsL = new ArrayList<String>();
+		boolean rapidRefresh = false;
+		boolean onlyWunder = false;
+		boolean subHour = false;
+		File hrrrCtlFile = new File(xsTmp+"/grib2/HRRR.ctl");
+		File hrrrCtlSubFile = new File(xsTmp+"/grib2/HRRRsub.ctl");
+		File hrrrGrib2File = new File(xsTmp+"/grib2/HRRR");
+		File hrrrGrib2SubFile = new File(xsTmp+"/grib2/HRRRsub");
+		String subHourFlag = "no";
+		
+        gradsOutObj.mkdirs();
+        wwwOutObj.mkdirs();
+
+		if(args.length > 0 && args[0].equals("Rapid")) { rapidRefresh = true; subHour = true; subHourFlag = "yes"; onlyWunder = false; }
+		if(args.length > 0 && args[0].equals("Wunder")) { rapidRefresh = true; subHour = false; subHourFlag = "no"; onlyWunder = true; }
+
+		System.out.println(" -> DEBUG: (String) getHour = "+getHour);
+		System.out.println(" -> DEBUG: (String) getHourMin = "+getHourMin);
+		System.out.println(" -> DEBUG: (String) getDate = "+getDate);
+                
+		if(!onlyWunder) {
+			
+			/* URLs! */
+			String hrrrGrib2URL = modelBeans.getNomadsBase() + "/hrrr/prod/hrrr."+getDate+"/conus/hrrr.t"+getHour+"z.wrfprsf"+tFHour2D+".grib2";
+			if(subHour) {
+				hrrrCtlFile = hrrrCtlSubFile;
+				hrrrGrib2URL = modelBeans.getNomadsBase() + "/hrrr/prod/hrrr."+getDate+"/conus/hrrr.t"+getHour+"z.wrfsubhf"+tSFHour2D+".grib2";
+				hrrrGrib2File = hrrrGrib2SubFile;
+			}
+			final String metarsURL = "http://aviationweather.gov/adds/dataserver_current/current/metars.cache.xml.gz";
+			final String xmlObsURL = "http://w1.weather.gov/xml/current_obs/all_xml.zip";
+
+			if(!rapidRefresh) {
+				wc.deleteDir(xsTmpObj);
+				xsTmpObj.mkdirs();
+			}
+		
+            try ( ResultSet resultSetGVars = wc.q2rs1c(dbc, gVarsSQL, null); ) {
+                    while (resultSetGVars.next()) { gVars.add(resultSetGVars.getString("gVar")); }
+            } catch (Exception e) { e.printStackTrace(); }
+
+            try ( ResultSet resultSetGVarsH = wc.q2rs1c(dbc, gVarsHSQL, null); ) {
+                    while (resultSetGVarsH.next()) { gVarsH.add(resultSetGVarsH.getString("gVar")); }
+            } catch (Exception e) { e.printStackTrace(); }
+
+            try ( ResultSet resultSetGVarsL = wc.q2rs1c(dbc, gVarsLSQL, null); ) {
+                    while (resultSetGVarsL.next()) { gVarsL.add(resultSetGVarsL.getString("gVar")); }
+            } catch (Exception e) { e.printStackTrace(); }
+
+            for (String thisGVar : gVars) {
+                    File thisGVarPath = new File(gradsOutObj.getPath()+"/"+thisGVar);
+                    File thisGVarWPath = new File(wwwOutObj.getPath()+"/"+thisGVar);
+                    thisGVarPath.mkdirs();
+                    thisGVarWPath.mkdirs();
+            }
+                        
+			wc.jsoupOutBinary(hrrrGrib2URL, hrrrGrib2File, modelBeans.getDownloadTimeout());
+
+			wc.runProcess("(\""+appPath+"/wgrib2\" "+hrrrGrib2File.getPath()+" -pdt | egrep -v \"^600:\" | \""+appPath+"/wgrib2\" -i "+hrrrGrib2File.getPath()+" -grib "+hrrrGrib2File.getPath()+")");
+			wc.runProcess("(\""+appPath+"/g2ctl\" "+hrrrGrib2File.getPath()+" > "+hrrrCtlFile.getPath()+")");
+			wc.runProcess("\""+appPath+"/gribmap\" -v -i "+hrrrCtlFile.getPath());
+			
+			Thread d1a = new Thread(() -> { wc.jsoupOutBinary(xmlObsURL, nwsObsXMLzipFile, 15.0); });
+			Thread d1b = new Thread(() -> { wc.jsoupOutBinary(metarsURL, metarsZipFile, 15.0); });
+			Thread dList1[] = { d1a, d1b };
+			for (Thread thread : dList1) { thread.start(); }
+			for (int i = 0; i < dList1.length; i++) { try { dList1[i].join(); } catch (InterruptedException nx) { nx.printStackTrace(); } }
+
+			Thread d2a = new Thread(() -> { wc.unzipFile(nwsObsXMLzipFile.getPath(), xsTmp); });
+			Thread d2b = new Thread(() -> { wc.runProcess("gunzip \""+metarsZipFile.getPath()+"\""); });
+			Thread dList2[] = { d2a, d2b };
+			for (Thread thread : dList2) { thread.start(); }
+			for (int i = 0; i < dList2.length; i++) { try { dList2[i].join(); } catch (InterruptedException nx) { nx.printStackTrace(); } }
+	                         
+            if(debugMode) {
+
+                System.out.println("DEBUG MODE ON!");
+                xswf.stations(true, xsTmp, "USC");
+
+            } else {
+
+                wc.runProcess("(echo \"run "+helpers.getPath()+"/xsGraphics.gs "+getDate+" "+getHourMin+" "+gradsOutObj.getPath()+" "+subHourFlag+"\" | \""+appPath+"/grads\" -blc \"open "+hrrrCtlFile.getPath()+"\" &>> "+logFile.getPath()+")");
+                for (String gVar : gVarsH) { wc.runProcess("convert \""+gradsOutObj.getPath()+"/"+gVar+"/"+getDate+"_"+getHourMin+"_"+gVar+".png\" -gravity Center -crop "+resHigh+"+0+0 "+gradsOutObj.getPath()+"/"+gVar+"/"+getDate+"_"+getHourMin+"_"+gVar+".png"); }
+                for (String gVar : gVarsL) { wc.runProcess("convert \""+gradsOutObj.getPath()+"/"+gVar+"/"+getDate+"_"+getHourMin+"_"+gVar+".png\" -gravity Center -crop "+resLow+"+0+0 "+gradsOutObj.getPath()+"/"+gVar+"/"+getDate+"_"+getHourMin+"_"+gVar+".png"); }
+
+                xsio.main(true, gVars, gVarsH, gVarsL);
+                xsmaa.main(true);
+
+                Thread xs1 = new Thread(() -> { xswf.stations(debugMode, xsTmp, "USC"); });
+                Thread xs2 = new Thread(() -> { xswf.stations(debugMode, xsTmp, "USE"); });
+                Thread xs3 = new Thread(() -> { xswf.stations(debugMode, xsTmp, "USW"); });
+                Thread xs4 = new Thread(() -> { xswb.stations(xsTmp, "None"); });
+                Thread xs5 = new Thread(() -> { xswm.main(xsTmp); });
+                Thread xs6 = new Thread(() -> { xswy.main(xsTmp, "None"); });
+                Thread xs7 = new Thread(() -> { xswh.main(xsTmp, "None"); });
+                Thread xs8 = new Thread(() -> { xsww.main(xsTmp, "None"); });
+                Thread xsPool[] = { xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8 }; 
+                for (Thread thread : xsPool) { thread.start(); }
+                for (int i = 0; i < xsPool.length; i++) { try { xsPool[i].join(); } catch (InterruptedException nx) { nx.printStackTrace(); } }
+                
+                String jsonBigString = null;
+                try { jsonBigString = wc.runProcessOutVar("cat "+xsTmp+"/output_*.json"); } catch (IOException ix) { ix.printStackTrace(); }
+                jsonBigString = ("{"+jsonBigString+"}").replace("\n","").replace(",}", "}");
+                try { wc.varToFile(jsonBigString, jsonDebugDumpFile, false); } catch (FileNotFoundException fnf) { fnf.printStackTrace(); }
+                String jsonBigSQLQuery = "INSERT INTO WxObs.StationDataIndexed (jsonData) VALUES ('"+jsonBigString+"');";
+                try { wc.q2do1c(dbc, jsonBigSQLQuery, null); } catch (Exception e) { e.printStackTrace(); }
+
+            }
+			
+		} else {
+			
+			xsww.main(xsTmp, "None");
+			
+		}
+		
+		String jsonRapidString = null;
+		try { jsonRapidString = wc.runProcessOutVar("cat "+xsTmp+"/rapid_*.json"); } catch (IOException ix) { ix.printStackTrace(); }
+		jsonRapidString = ("{"+jsonRapidString+"}").replace("\n","").replace(",}", "}");
+		try { wc.varToFile(jsonRapidString, jsonDebugDumpRapidFile, false); } catch (FileNotFoundException fnf) { fnf.printStackTrace(); }
+		String jsonRapidSQLQuery = "INSERT INTO WxObs.RapidSDI (jsonData) VALUES ('"+jsonRapidString+"');";
+		try { wc.q2do1c(dbc, jsonRapidSQLQuery, null); } catch (Exception e) { e.printStackTrace(); }
+
+		final long endTime = System.currentTimeMillis();
+	
+		long totalRunTime = (endTime - startTime)/1000;
+
+		String xsRuntime = "INSERT INTO WxObs.Logs VALUES (Null,"+totalRunTime+");";
+		try { wc.varToFile(xsRuntime, logFile, true); } catch (FileNotFoundException fnf) { fnf.printStackTrace(); }
+		try { wc.q2do1c(dbc, xsRuntime, null); } catch (Exception e) { e.printStackTrace(); }
+
+		System.out.println("Updates completed! Runtime: "+totalRunTime); 
+        
+        try { dbc.close(); } catch (Exception e) { e.printStackTrace(); }
+		
+	}
+
+}
